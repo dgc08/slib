f982dcc83de651d137cd373ac253caebe3e4ce7a
Sinthoras39 <digc0820@gmail.com>
2024-07-27 22:15:47 +0200

    Time part

---
+++ src/time.c
@@ -0,0 +1,17 @@
+#include "../time.h"
+
+#include <time.h>
+#include <unistd.h>
+
+void delay(float msecs) {
+    usleep(msecs*1000);
+}
+
+void get_time(int* hours, int* minutes, int* seconds) {
+    time_t now = time(NULL);
+    struct tm* now_tm = localtime(&now);
+
+    *hours = now_tm->tm_hour;
+    *minutes = now_tm->tm_min;
+    *seconds = now_tm->tm_sec;
+}
---
+++ time.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_TIME_H_
+#define UTILS_TIME_H_
+
+void delay(float msecs);
+void get_time(int* hours, int* minutes, int* seconds);
+
+
+#endif // UTILS_TIME_H_
